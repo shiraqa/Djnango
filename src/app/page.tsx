3c0886279683cb1bc2865910a87cee913a41ee98
shiraqa <shiraqa.ahmady786@gmail.com>
2024-07-03 23:01:57 +0430

    add counter button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+'use client'
+import { useState } from "react";
 export default function Home() {
+  const [counter, setCounter] = useState(0);
   return (
     <>
-      <h1>test page</h1>
+    <div className="flex justify-center p-2 content-center">
+      <button className=" border p-2 bg-slate-400 rounded-lg" onClick={()=>setCounter(counter+1)}>Counter - {counter}</button>
+      </div>
     </>
   );
 }
